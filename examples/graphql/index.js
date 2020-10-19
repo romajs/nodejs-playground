65a7edd3d2cdf21af8891660d727f700526b20b4
Romualdo Santos <romajslit@gmail.com>
2020-10-19 15:21:32 -0300

    feat(graphql): adding examples, raw, express and apollo

---
+++ examples/graphql/index.js
@@ -0,0 +1,13 @@
+var { buildSchema, graphql } = require('graphql');
+
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello world!' };
+
+graphql(schema, '{ hello }', root).then((response) => {
+  console.log(response);
+});
