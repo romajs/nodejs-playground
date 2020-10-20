2697d2ad55b2c05829e146ccb58a5eedacc6570a
Romualdo Santos <romajslit@gmail.com>
2020-10-20 18:23:11 -0300

    feat(typeorm): main (index) refactoring

--- examples/typeorm/src/index.ts
+++ examples/typeorm/src/index.ts
@@ -2,71 +2,76 @@ import 'reflect-metadata';
 
 import * as bodyParser from 'body-parser';
 
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 import { Request, Response } from 'express';
-import { createConnection, getConnectionOptions } from 'typeorm';
 
 import { Routes } from './routes';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { User } from './entity/User';
 import express from 'express';
 
-getConnectionOptions().then((connectionOptions) => {
-  return createConnection(
-    Object.assign(connectionOptions, {
-      namingStrategy: new SnakeNamingStrategy(),
-    })
-  )
-    .then(async (connection) => {
-      // create express app
-      const app = express();
-      app.use(bodyParser.json());
+const startServer = () => {
+  // create express app
+  const app = express();
+  app.use(bodyParser.json());
 
-      // register express routes from defined application routes
-      Routes.forEach((route) => {
-        (app as any)[route.method](
-          route.route,
-          (req: Request, res: Response, next: Function) => {
-            const result = new (route.controller as any)()[route.action](
-              req,
-              res,
-              next
-            );
-            if (result instanceof Promise) {
-              result.then((result) =>
-                result !== null && result !== undefined
-                  ? res.send(result)
-                  : undefined
-              );
-            } else if (result !== null && result !== undefined) {
-              res.json(result);
-            }
-          }
+  // register express routes from defined application routes
+  Routes.forEach((route) => {
+    (app as any)[route.method](
+      route.route,
+      (req: Request, res: Response, next: Function) => {
+        const result = new (route.controller as any)()[route.action](
+          req,
+          res,
+          next
         );
-      });
+        if (result instanceof Promise) {
+          result.then((result) =>
+            result !== null && result !== undefined
+              ? res.send(result)
+              : undefined
+          );
+        } else if (result !== null && result !== undefined) {
+          res.json(result);
+        }
+      }
+    );
+  });
 
-      // setup express app here
-      // ...
+  // setup express app here
+  // ...
 
-      // start express server
-      app.listen(3000);
+  // start express server
+  app.listen(3000);
 
-      // insert new users for test
-      await connection.manager.save(
-        connection.manager.create(User, {
-          name: 'Timber',
-          age: 27,
-        })
-      );
-      await connection.manager.save(
-        connection.manager.create(User, {
-          name: 'Phantom',
-          age: 24,
-        })
-      );
+  console.log(
+    'Express server has started on port 3000. Open http://localhost:3000/users to see results'
+  );
+};
 
-      console.log(
-        'Express server has started on port 3000. Open http://localhost:3000/users to see results'
-      );
+const seed = async (connection) => {
+  await connection.manager.save(
+    connection.manager.create(User, {
+      name: 'Timber',
+      age: 27,
     })
-    .catch((error) => console.log(error));
-});
+  );
+  await connection.manager.save(
+    connection.manager.create(User, {
+      name: 'Phantom',
+      age: 24,
+    })
+  );
+};
+
+const main = async (): Promise<Connection> => {
+  const connectionOptions = Object.assign(await getConnectionOptions(), {
+    namingStrategy: new SnakeNamingStrategy(),
+  });
+  const connection: Connection = await createConnection(connectionOptions);
+  await startServer();
+  await seed(connection);
+  return connection;
+};
+
+main().catch((error) => console.log(error));
