a953444cd0983f7ffb41e8c37f5b7ed52a8c0e7b
Romualdo Santos <romajslit@gmail.com>
2020-10-24 00:17:58 -0300

    feat(graphql-express): adding jit

--- examples/graphql-express/graphql/schema.js
+++ examples/graphql-express/graphql/schema.js
@@ -2,7 +2,6 @@ const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
 const { makeExecutableSchema } = require('@graphql-tools/schema');
-const { mergeSchemas } = require('@graphql-tools/merge');
 
 module.exports = makeExecutableSchema({
   typeDefs,
--- examples/graphql-express/index.js
+++ examples/graphql-express/index.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./graphql/schema');
+const { compileQuery, isCompiledQuery } = require('graphql-jit');
+const { parse } = require('graphql');
+const bodyParser = require('body-parser');
 
 var app = express();
 
+const cache = {};
+
 app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
+
+app.use(bodyParser.json());
+app.use('/graphql-jit', async (req, res) => {
+  try {
+    const { query } = req.body;
+
+    const document = parse(query);
+
+    cache[query] = cache[query] || compileQuery(schema, document);
+
+    if (!isCompiledQuery(cache[query])) {
+      throw new Error('Error compiling query:', cache[query]);
+    }
+
+    const result = await cache[query].query();
+
+    res.end(JSON.stringify(result));
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
 
 app.listen(4000, () =>
   console.log('Now browse to http://localhost:4000/graphql')
